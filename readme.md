649752988cbea895d1b2092655a8bfa8ff46d33a
beauwilliams <beau.williams@icloud.com>
2021-01-19 16:08:07 +1100

    Committing initial files

---
+++ readme.md
@@ -0,0 +1,5 @@
+# Python Based Desktop Notifier for MAC OSX
+
+### I have rewritten the MAC OSX API in the plyer library as the version they use is now deprecated. This is a native AppleScript based service instead of reliant on pyobjc module
+
+### This is a basic template now for a further project
---
+++ test_notifier.py
@@ -0,0 +1,17 @@
+
+import time
+from plyer import notification
+
+
+if __name__=="__main__":
+
+        notification.notify(
+            title = "MACS",
+            message=" Quick shoutout to the MACS fam" ,
+
+            # displaying time
+            timeout=2
+)
+        # waiting time
+        time.sleep(7)
+
